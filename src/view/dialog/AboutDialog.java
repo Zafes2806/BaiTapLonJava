98c01839eb94d2d60e83f1e8c172c2179b284793
Zafes2806 <manhha280699@gmail.com>
2024-05-02 10:10:18 +0700

    chinh sua giao dien AboutDialog

--- src/view/dialog/AboutDialog.java
+++ src/view/dialog/AboutDialog.java
@@ -5,8 +5,6 @@ import java.awt.Graphics;
 import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
